67f9bbee50679fbe6d10d69039752699453f8ef0
felicienfouillet <felicienfouillet@live.fr>
2022-06-15 12:09:09 +0200

    (evo): init deliveries api

---
+++ deliveries/src/services/deliveries.service.ts
@@ -0,0 +1,77 @@
+import { Delivery, IDelivery } from 'models/deliveries.model';
+import { NotFoundException } from 'utils/exceptions';
+
+export class DeliveriesService {
+    /**
+     * Trouve tous les deliveries
+     */
+    async findAll(): Promise<Array<IDelivery>> {
+        try {
+            const deliveries = await Delivery.find();
+
+            return deliveries;
+        } catch (e) {
+            throw new NotFoundException('No deliveries found');
+        }
+    }
+
+    /**
+     * Trouve un delivery en particulier
+     * @param id - ID unique de l'delivery
+     */
+    async findOne(id: string): Promise<IDelivery | null | undefined> {
+        try {
+            const delivery = await Delivery.findById(id);
+
+            return delivery;
+        } catch (e) {
+            throw new NotFoundException('No delivery found');
+        }
+    }
+
+    /**
+     * Met à jour un delivery en particulier
+     *
+     * /!\ Idéalement, il faudrait vérifier le contenu de la requête avant de le sauvegarder.
+     *
+     * @param deliveryData - Un objet correspondant à un delivery, il ne contient pas forcément tout un delivery. Attention, on ne prend pas l'id avec.
+     * @param id - ID unique de l'delivery
+     */
+    async update(id: string, deliveryData: Partial<IDelivery>): Promise<IDelivery | null | undefined> {
+        const delivery = await this.findOne(id);
+
+        if (!delivery) {
+            throw new NotFoundException('No delivery found');
+        }
+
+        const updatedDelivery = await Delivery.findByIdAndUpdate(id, deliveryData, { new: true });
+
+        return updatedDelivery;
+    }
+
+    /**
+     * Créé un delivery
+     *
+     * /!\ Idéalement, il faudrait vérifier le contenu de la requête avant de le sauvegarder.
+     *
+     * @param deliveryData - Un objet correspondant à un delivery. Attention, on ne prend pas l'id avec.
+     */
+    async create(deliveryData: IDelivery): Promise<IDelivery> {
+        const newDelivery: IDelivery = await Delivery.create(deliveryData);
+
+        return newDelivery;
+    }
+
+    /**
+     * Suppression d'un delivery
+     */
+    async delete(id: string) {
+        const delivery = await this.findOne(id);
+
+        if (!delivery) {
+            throw new NotFoundException('No delivery found');
+        }
+
+        await Delivery.findByIdAndRemove(id);
+    }
+}
