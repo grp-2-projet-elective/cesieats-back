1460b1a87e6105d3ad555ceaaf8d04cb8fa425ae
EnzoLEBRUN <44493282+EnzoLEBRUN@users.noreply.github.com>
2022-06-27 21:03:16 +0200

    (fix): Environment (#34)

--- menus/src/environment/environment.ts
+++ menus/src/environment/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
     API_PORT: 4600,
     USERS_API_PORT: 4100,
     USERS_API_HOSTNAME: 'localhost',
-    MONGO_URI: '@vps.felicien-fouillet.fr:27017/orders?readPreference=nearest&directConnection=true&authSource=admin',
+    MONGO_URI: '@vps.felicien-fouillet.fr:27017/menus?readPreference=nearest&directConnection=true&authSource=admin',
     ACCESS_TOKEN: 'd53ee3ae836863ff8ca29dd0232ad93a74ee63a384af1ce9364fd1df85a19579'
 }
