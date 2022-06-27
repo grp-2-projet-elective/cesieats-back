4cb284421fcdd20d4b99a067124f5f4112e5f9fe
felicienfouillet <felicienfouillet@live.fr>
2022-06-27 23:59:23 +0200

    (evo): update cesieats-helpers package + (refacto): refacto of users api for single user requesting

--- restaurants/src/middlewares/restaurant-auth.middleware.ts
+++ restaurants/src/middlewares/restaurant-auth.middleware.ts
@@ -32,34 +32,6 @@ export abstract class RestaurantAuthMiddleware {
         }
     }
 
-    public static async verifyRestaurantOwnership(req: Request, res: Response, next: NextFunction) {
-        if ((req as any).skipMiddlewares) {
-            return next();
-        }
-
-        try {
-            const mail: string = req.body.mail;
-            const restaurantId: string = req.body.restaurantId;
-
-            if (!mail) {
-                return res.status(400).send({ message: 'User mail not provided' });
-            }
-
-            const isProfileOwner: boolean = await RestaurantsService.haveRestaurantOwnership(mail, restaurantId);
-
-            if (!isProfileOwner) {
-                return res.status(400).send({ message: 'Unauthorized' });
-            }
-
-            return next();
-        } catch (e: any) {
-            console.error(e);
-            return res
-                .status(e.status ? e.status : 500)
-                .json(e);
-        }
-    }
-
     public static async isApiCall(req: Request, res: Response, next: NextFunction) {
         const hostname = req.hostname;
         console.log('API call from: ', hostname);
